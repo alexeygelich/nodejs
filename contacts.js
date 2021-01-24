965485bedad1fe3c3854e437267f26c2014b9e5e
Aleksey Gelich <alexeygelich@gmail.com>
2021-01-24 21:27:10 +0200

    fix

--- contacts.js
+++ contacts.js
@@ -3,49 +3,50 @@ const path = require("path");
 
 const contactsPath = path.join("./db", "/contacts.json");
 // TODO: задокументировать каждую функцию
-function listContacts() {
-  return fs
-    .readFile(contactsPath, "utf-8")
-    .then((data) => {
-      console.table(JSON.parse(data.toString()));
-      return data.toString();
-    })
-    .catch((err) => console.log("err ", err));
+async function listContacts() {
+  let listContact;
+
+  try {
+    listContact = await fs.readFile(contactsPath, "utf-8");
+  } catch (error) {
+    console.log(error);
+  }
+
+  const listContactArr = JSON.parse(listContact.toString());
+  console.table(listContactArr);
+
+  return listContactArr;
 }
 
 async function getContactById(contactId) {
   const listContact = await listContacts();
-  const listContactArr = JSON.parse(listContact);
-  const contact = listContactArr.find((contact) => contact.id === contactId);
+  const contact = listContact.find((contact) => contact.id === contactId);
   console.table(contact);
-  return contact;
 }
 
 async function removeContact(contactId) {
   const listContact = await listContacts();
-  const listContactArr = JSON.parse(listContact);
-  const listContactFiltered = listContactArr.filter((contact) => contact.id !== contactId);
-  wrightToFile(listContactFiltered);
-  listContacts();
+  const listContactFiltered = listContact.filter((contact) => contact.id !== contactId);
+  await wrightToFile(listContactFiltered);
+  await listContacts();
 }
 
 async function addContact(name, email, phone) {
   const listContact = await listContacts();
-  const listContactArr = JSON.parse(listContact);
-  const getIds = listContactArr.map((contact) => contact.id);
+  const getIds = listContact.map((contact) => contact.id);
   let id = 0;
   for (let i of getIds) {
     if (i > id) id = i;
   }
   id++;
-  listContactArr.push({ id, name, email, phone });
-  wrightToFile(listContactArr);
-  listContacts();
+  listContact.push({ id, name, email, phone });
+  await wrightToFile(listContact);
+  await listContacts();
 }
 
-function wrightToFile(array) {
+async function wrightToFile(array) {
   const arrayStr = JSON.stringify(array);
-  fs.writeFile(contactsPath, arrayStr, (err) => {
+  await fs.writeFile(contactsPath, arrayStr, (err) => {
     if (err) console.log("err ", err);
   });
 }
