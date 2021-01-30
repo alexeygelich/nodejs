d97a41f50d339c22be231bbab80f4a18c0c0d0a7
Aleksey Gelich <alexeygelich@gmail.com>
2021-01-30 23:40:34 +0200

    second homework

--- index.js
+++ index.js
@@ -1,49 +1,41 @@
 const express = require("express");
-const fetch = require("node-fetch");
-const Joi = require("joi");
-const dotenv = require("dotenv");
 const cors = require("cors");
-
-dotenv.config();
+const contactRoutes = require("./routes/contact.routes");
 
 const PORT = process.env.PORT || 8080;
 
-const app = express();
-
-const API_KEY = process.env.OPEN_WEATHER_API_KEY;
-
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-app.get("/weather", validateWeatherQueryParams, async (req, res) => {
-  const {
-    query: { lat, lon },
-  } = req;
-
-  const result = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
-
-  const data = await result.json();
-  res.json(data);
-});
-
-function validateWeatherQueryParams(req, res, next) {
-  const validationRules = Joi.object({
-    lat: Joi.string().required(),
-    lon: Joi.string().required(),
-  });
-
-  const validationResult = validationRules.validate(req.query);
-
-  if (validationResult.error) {
-    return res.status(400).send(validationResult.error);
+class Server {
+  constructor() {
+    this.server = null;
   }
 
-  next();
+  start() {
+    this.server = express();
+    this.initMiddlewares();
+    this.initRoutes();
+    this.listen();
+  }
+
+  initMiddlewares() {
+    this.server.use(express.json());
+    this.server.use(
+      cors({
+        origin: "*",
+      })
+    );
+  }
+
+  initRoutes() {
+    this.server.use("/contacts", contactRoutes);
+  }
+
+  listen() {
+    this.server.listen(PORT, () => {
+      console.log("Server is listening on port: ", PORT);
+    });
+  }
 }
 
-app.listen(PORT, () => {
-  console.log("сервер работает на порту: ", PORT);
-});
+const server = new Server();
+
+server.start();
---
+++ models/Contact.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Adam",
+    email: "adam@qwe.com",
+    phone: "099 999 44 88",
+  },
+  {
+    id: 2,
+    name: "Eva",
+    email: "Eva@qwe.com",
+    phone: "099 999 44 88",
+  },
+  {
+    id: 3,
+    name: "Jhon",
+    email: "Jhon@qwe.com",
+    phone: "099 999 44 88",
+  },
+];
