12511ab4b1750885f905699f997e54adb70a2774
Aleksey Gelich <alexeygelich@gmail.com>
2021-02-08 17:09:27 +0200

    3 dz

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const cors = require("cors");
 const contactRoutes = require("./routes/contact.routes");
+
+dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
@@ -12,8 +16,22 @@ class Server {
   start() {
     this.server = express();
     this.initMiddlewares();
+    this.connectToDb();
     this.initRoutes();
     this.listen();
+  }
+
+  async connectToDb() {
+    try {
+      const data = await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Database connection successful");
+    } catch (err) {
+      console.log(err);
+      process.exit(1);
+    }
   }
 
   initMiddlewares() {
